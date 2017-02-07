b313b0d2a39d62c6b3d22504b809e77258d92d9e
saibot777 <stefantrajkovic777@gmail.com>
2017-02-07 14:53:19 +0100

    ngRoute created...

--- app/app.js
+++ app/app.js
@@ -5,5 +5,21 @@
     "use strict";
     var app = angular.module("productManagement",
                             ["common.services",
+                             "ui.router",
                             "productResourceMock"]);
+    
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+                function ($stateProvider, $urlRouterProvider) {
+                    $urlRouterProvider.otherwise("/products");
+                    $stateProvider
+                    // Products
+                    .state("productList", {
+                        url: "/products",
+                        templateUrl: "app/products/productListView.html",
+                        controller: "ProductListCtrl as vm"
+                    })
+                }]
+        
+    );
 }());
